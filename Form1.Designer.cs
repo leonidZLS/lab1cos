8008cbc3977654d5d2e86e30b6b01a75cd437edc
leonidZLS <leonwotz@mail.ru>
2020-10-17 00:46:11 +0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.buttonOpen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonOpen.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOpen.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonOpen.Location = new System.Drawing.Point(11, 11);
+            this.buttonOpen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(170, 37);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Открыть файл";
+            this.buttonOpen.UseVisualStyleBackColor = false;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(9, 74);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(209, 84);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 204);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.buttonOpen);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public String fileSignature;
+        public Int32 fileSize;
+        public Int16 reserved1;
+        public Int16 reserved2;
+        public Int32 locationOfData;
+        public Int32 sizeHeader;
+        public Int32 imgWidth;
+        public Int32 imgHeight;
+        public Int16 numberPlanes;
+        public Int16 bitPixel;
+        public Int32 bfCompress;
+        public Int32 sizeRasterArray;
+        public Int32 imgHorizontalResolution;
+        public Int32 imgVerticalResolution;
+        public Int32 numberColors;
+        public Int32 numberMainColors;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog.Filter = "bmp |*.bmp";
+            this.openFileDialog.ShowDialog();
+
+            BinaryReader bReader = new BinaryReader(File.Open(openFileDialog.FileName, FileMode.Open));
+            fileSignature = new string(bReader.ReadChars(2));
+            fileSize = bReader.ReadInt32();
+            reserved1 = bReader.ReadInt16();
+            reserved2 = bReader.ReadInt16();
+            locationOfData = bReader.ReadInt32();
+            sizeHeader = bReader.ReadInt32();
+            imgWidth = bReader.ReadInt32();
+            imgHeight = bReader.ReadInt32();
+            numberPlanes = bReader.ReadInt16();
+            bitPixel = bReader.ReadInt16();
+            bfCompress = bReader.ReadInt32();
+            sizeRasterArray = bReader.ReadInt32();
+            imgHorizontalResolution = bReader.ReadInt32();
+            imgVerticalResolution = bReader.ReadInt32();
+            numberColors = bReader.ReadInt32();
+            numberMainColors = bReader.ReadInt32();
+
+
+            bReader.Close();
+
+            String compressType = 0.ToString();
+            if (bfCompress == 0 || bfCompress == 3 || bfCompress == 6)
+                compressType = "Без сжатия";
+            else if (bfCompress == 1 || bfCompress == 2)
+                compressType = "RLE";
+            else if (bfCompress == 4)
+                compressType = "JPEG";
+            else if (bfCompress == 5)
+                compressType = "PNG";
+
+            Bitmap original_image = new Bitmap(openFileDialog.FileName);
+            this.pictureBox.Width = original_image.Width;
+            this.pictureBox.Height = original_image.Height;
+            this.pictureBox.Image = original_image;
+            this.Location = new Point(0, 0);
+            this.Size = new Size(original_image.Width + 400, original_image.Height + 150);
+            this.pictureBox.Show();
+
+
+            String message = "Сигнатура файла: " + fileSignature + "\n Размер файла: " + fileSize.ToString() +
+                             "\n Местонахождение данных растрового массива: " + locationOfData.ToString() +
+                             "\n Длина заголовка растрового массива: " + sizeHeader.ToString() +
+                             "\n Ширина изображения: " + imgWidth.ToString() + "\n Высота изображения: " +
+                             imgHeight.ToString() + "\n Число цевтовых плоскостей: " + numberPlanes +
+                             "\n Бит/пиксел: " + bitPixel + "\n Метод сжатия: " + compressType +
+                             "\n Длина растрового массива: " + sizeRasterArray + "\n Горизонтальное разрешение: " +
+                             imgHorizontalResolution + "\n Вертикальное разрешение: " + imgVerticalResolution +
+                             "\n Количество цветов изображения: " + numberColors + "\n Количество основных цветов: " +
+                             numberMainColors;
+
+            MessageBox.Show(message);
+        }
+    }
+}
